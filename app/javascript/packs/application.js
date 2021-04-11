13246f30d3ccde08275b6c51244a6c6453d3b0f7
ilya129 <ilya129129@gmail.com>
2021-04-11 12:05:25 +0300

    Good version

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,30 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+// This is a manifest file that'll be compiled into application.js, which will
+// include all the files listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts,
+// vendor/assets/javascripts, or any plugin's vendor/assets/javascripts
+// directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear
+// at the bottom of the compiled file.
+//
+// Read Sprockets README for details about supported directives.
+//
+// https://github.com/rails/sprockets#sprockets-directives
+//
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// Этот код добавляет обработчик события выдвигания формы при клике на кнопку
+// «Задать вопрос», которая видна только, когда страница открыта в маленьком
+// окне.
+require('jquery')
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+$(function(){
+  $('#ask-button').click(function(){
+    $('#ask-form').slideToggle(300);
+    return false;
+  });
+});
+
--- db/schema.rb
+++ db/schema.rb
@@ -18,9 +18,8 @@ ActiveRecord::Schema.define(version: 2021_04_02_122624) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
-
-  add_index "questions", ["user_id"], name: "index_questions_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string "name"
