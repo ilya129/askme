c461ae0fdbc50118c266299fdb10ea4db4f45d8f
ilya129 <ilya129129@gmail.com>
2021-06-14 14:15:37 +0300

    dom_1 version 3.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest::SHA256.new
   CHECKING_EMAIL = /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
-  CHECKING_USERNAME = /[a-zA-Z0-9_]+/
+  CHECKING_USERNAME = /\w/
 
   attr_accessor :password
 
@@ -18,25 +18,9 @@ class User < ApplicationRecord
   validates :username, length: { maximum: 40 }
   validates :username, format: CHECKING_USERNAME
 
-  validates :password, :presence =>true, :confirmation =>true
+  validates :password, presence: true, confirmation: true
 
   before_save :encrypt_password
-
-  def encrypt_password
-    if password.present?
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-
-      self.password_hash = User.hash_to_string(
-        OpenSSL::PKCS5.pbkdf2_hmac(
-          password, password_salt, ITERATIONS, DIGEST.length, DIGEST
-        )
-      )
-    end
-  end
-
-  def self.hash_to_string(password_hash)
-    password_hash.unpack('H*')[0]
-  end
 
   def self.authenticate(email, password)
     user = find_by(email: email)
@@ -52,5 +36,23 @@ class User < ApplicationRecord
     return user if user.password_hash == hashed_password
 
     nil
+  end
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(
+          password, password_salt, ITERATIONS, DIGEST.length, DIGEST
+        )
+      )
+    end
+  end
+
+  private
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
   end
 end
