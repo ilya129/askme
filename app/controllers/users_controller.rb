8e93329187e071a119b907bf9686fa3d2b7340d4
ilya129 <ilya129129@gmail.com>
2021-06-22 15:30:45 +0300

    dom_4 version 1.0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
 
     if @user.save
       redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+      session[:user_id] = @user.id
     else
       render 'new'
     end
