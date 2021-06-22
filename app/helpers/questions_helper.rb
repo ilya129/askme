0374bbb9c5a473dfc955645ae43aa529a7332e9f
ilya129 <ilya129129@gmail.com>
2021-06-22 15:11:43 +0300

    dom_3 fixed version 2.1

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -4,8 +4,10 @@ module QuestionsHelper
     prefix = with_number ? "#{number} " : ''
     return prefix + many if (number % 100).between?(11, 14)
     word = case number % 10
-    when 1 then one
-    when 2..4 then few
+    when 1
+      one
+    when 2..4
+      few
     else
       many
     end
