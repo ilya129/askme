0e0340826f7cb05c08201178ebefac34fd7318b5
ilya129 <ilya129129@yandex.ru>
2021-07-19 18:40:17 +0300

    dom_6 deleting_a_user version 1.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   attr_accessor :password
 
-  has_many :questions
+  has_many :questions, dependent: :delete_all
 
   validates :email, :username, presence: true
   validates :password, presence: true, on: :create
